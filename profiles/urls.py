93ee616415779d8154c8b2c0e0f05ca4a66dcc53
Ayush Anshul <ayushanshul07@gmail.com>
2020-05-15 02:50:23 +0530

    refactor code and fix a security issue

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('school/<int:school_id>/', views.schoolhome, name='homeschool'),
-    path('teacher/<int:teacher_id>/', views.teacherhome, name='hometeacher'),
-    path('student/<int:student_id>/', views.studenthome, name='homestudent')
+    path('school/<int:id>/', views.schoolhome, name='homeschool'),
+    path('teacher/<int:id>/', views.teacherhome, name='hometeacher'),
+    path('student/<int:id>/', views.studenthome, name='homestudent')
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -3,8 +3,21 @@ from django.shortcuts import render
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 from django.contrib.auth.models import User
+from django.contrib.auth import REDIRECT_FIELD_NAME
 from accounts.models import SchoolAdmin, Teacher, Student, Types
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.core.exceptions import PermissionDenied
+
+
+def same_user_check(function):
+    def wrap(request, *args, **kwargs):
+        if kwargs['id'] == request.user.id:
+            return function(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
+    wrap.__doc__ = function.__doc__
+    wrap.__name__ = function.__name__
+    return wrap
 
 
 def generate_username(first_name, last_name):
@@ -20,7 +33,8 @@ def generate_username(first_name, last_name):
 		x += 1
 
 @login_required(login_url='accounts/login/')
-def schoolhome(request, school_id):
+@same_user_check
+def schoolhome(request, id):
 	if request.method == 'POST':
 		for html_name, uploaded_file in request.FILES.items():
 			myfile = request.FILES[html_name]
@@ -31,7 +45,7 @@ def schoolhome(request, school_id):
 
 			# remove first 4 chars i.e class
 			standard = html_name[5:]
-			school_object = SchoolAdmin.objects.get(id=school_id)
+			school_object = SchoolAdmin.objects.get(id=id)
 			if standard == '':
 				Teacher.objects.all().delete()
 				with open(uploaded_file_url) as f:
@@ -45,16 +59,6 @@ def schoolhome(request, school_id):
 							password=generate_username(row[0],row[2]),
 							school_id=school_object,
 							role=Types.TEACHER)
-				school_object = SchoolAdmin.objects.get(id=school_id)
-				teachers_list = Teacher.objects.filter(school_id_id=school_id)
-				students_list = Student.objects.filter(school_id_id=school_id)
-				class_list = [5,6,7,8,9,10]
-				return render(request, 'profiles/schoolhome.html',
-					{	'school_admin': school_object, 
-						'teachers_list': teachers_list,
-						'students_list': students_list,
-						'class_list': class_list
-					})
 			else:
 				standard = int(standard)
 				Student.objects.filter(standard=standard).delete()
@@ -70,34 +74,23 @@ def schoolhome(request, school_id):
 							school_id=school_object,
 							standard=standard,
 							role=Types.STUDENT)
-				school_object = SchoolAdmin.objects.get(id=school_id)
-				teachers_list = Teacher.objects.filter(school_id_id=school_id)
-				students_list = Student.objects.filter(school_id_id=school_id)
-				class_list = [5,6,7,8,9,10]
-
-				return render(request, 'profiles/schoolhome.html',
-					{	'school_admin': school_object, 
-						'teachers_list': teachers_list,
-						'students_list': students_list,
-						'class_list': class_list
-					})	
-	else:
-		school_object = SchoolAdmin.objects.get(id=school_id)
-		teachers_list = Teacher.objects.filter(school_id_id=school_id)
-		students_list = Student.objects.filter(school_id_id=school_id)
-		class_list = [5,6,7,8,9,10]
-
-		return render(request, 'profiles/schoolhome.html',
-			{	'school_admin': school_object, 
-				'teachers_list': teachers_list,
-				'students_list': students_list,
-				'class_list': class_list
-			})
+	school_object = SchoolAdmin.objects.get(id=id)
+	teachers_list = Teacher.objects.filter(school_id_id=id)
+	students_list = Student.objects.filter(school_id_id=id)
+	class_list = [5,6,7,8,9,10]
+	return render(request, 'profiles/schoolhome.html',
+		{	'school_admin': school_object, 
+			'teachers_list': teachers_list,
+			'students_list': students_list,
+			'class_list': class_list
+		})
 
 @login_required(login_url='accounts/login/')
-def teacherhome(request, teacher_id):
+@same_user_check
+def teacherhome(request, id):
 	return render(request,'profiles/teacherhome.html')
 
 @login_required(login_url='accounts/login/')
-def studenthome(request, student_id):
+@same_user_check
+def studenthome(request, id):
 	return render(request,'profiles/studenthome.html')
